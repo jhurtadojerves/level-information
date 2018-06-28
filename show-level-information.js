ff5515910760f9b1da674e8207ff8247f1c427a9
Julio Hurtadoe <juliohurtado.backend@gmail.com>
2018-06-28 13:58:27 -0500

    Add total experience

--- show-level-information.js
+++ show-level-information.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show next level in every profile
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        http://www.harrylatino.org/user/*
